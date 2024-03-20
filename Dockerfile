7c17e08e3813e590546b03cbf68c6aa7b0b92057
mac <mac@MacBook-Pro-de-mac.local>
2024-03-20 16:45:14 -0400

    backend refactored

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR ./inward
 COPY . ./inward
 RUN pip install -r ./inward/requirements.txt
 RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
-CMD ["python3", "./inward/main.py"]
 EXPOSE 5000
+CMD ["python3", "./inward/main.py"]
--- workers/worker.py
+++ workers/worker.py
@@ -6,8 +6,9 @@ from rq import Worker, Queue, Connection
 
 listen = ['default']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-
+#redis_url = redis.Redis(host='redis', port=6379, decode_responses=True)
+#redis_url = os.getenv('REDISTOGO_URL', 'redis://172.17.0.1:6379')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://127.0.0.1:6379')
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
